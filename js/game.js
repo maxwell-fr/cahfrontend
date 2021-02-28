47a167ddf45a452c6896da452c4d12d0ab3585db
Cenecia <deviler151@gmail.com>
2021-02-27 19:55:23 -0500

    confetti!

--- js/game.js
+++ js/game.js
@@ -519,6 +519,20 @@ function selectCandidateCard(player){
     }
 }
 
+function shootConfetti()
+{
+    confetti({
+        particleCount: 100,
+        startVelocity: 30,
+        spread: 360,
+        origin: {
+            x: Math.random(),
+            // since they fall down, start a bit higher than random
+            y: Math.random() - 0.2
+        }
+    });
+}
+
 function gameOver(name){
     setGameOver();
     $("#gameDetails").html(name+" HAS WON THE GAME!");
