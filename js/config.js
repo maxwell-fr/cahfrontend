5670c90772774b695f52bf8b9bb6b71ea397d6b4
Maxwell French <maxwell@frenchnet.org>
2022-01-11 23:15:31 -0500

    Add more robust SSL configuration including reliable fallback if not configured. Add needed parameters to .env.

--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@
 //all API calls will start with this URL, e.g., `${CONFIG_BASEURL}/v1/games/getGame`
 export const CONFIG_API_HOST = "localhost";
 export const CONFIG_BASEURL = `https://${CONFIG_API_HOST}:3000`;
-export const CONFIG_WSURL = `ws://${CONFIG_API_HOST}:38080`;
+//use wss:// for SSL, ws:// for non-SSL
+export const CONFIG_WSURL = `wss://${CONFIG_API_HOST}:38080`;
 
 //This client works with this version of the server API
 export const EXPECTED_API_VERSION = {
