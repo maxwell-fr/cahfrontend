1a356e4020e1026e34395f98a11fb69daa280e98
Cenecia <deviler151@gmail.com>
2023-01-17 23:36:44 -0500

    include playerID and guid when picking winning card

--- js/game.js
+++ js/game.js
@@ -200,11 +200,9 @@ $("#newGame").on('click', function(){
                     name: game_name
                 },
                 success: function( result ) {
-                    addToConsole("Started new game: "+result.data.gameID);
                     setGameID(result.data.gameID);
-                    addToConsole("Your player ID: "+result.data.playerID);
-                    addToConsole("Your player guid: "+result.data.guid);
                     setPlayerID(result.data.playerID);
+                    setGUID(result.data.guid);
                     updatePlayers(result.data.players, null);
                     $(".gameIDtag").each(function (){
                         $(this).html("Game Link:");
@@ -277,10 +275,9 @@ $("#joinGame").on('click', function(){
         success: function( result ) {
             $("#whiteHand").html("");
             $("#gameBoard").html("");
-            addToConsole("Joined game ID: "+result.data.gameID);
             setGameID(result.data.gameID);
-            addToConsole("player ID: "+result.data.players[result.data.players.length-1]._id);
-            setPlayerID(result.data.players[result.data.players.length-1]._id);
+            setPlayerID(result.data.playerID);
+            setGUID(result.data.guid);
             updatePlayers(result.data.players, null);
             localStorage.removeItem("round");
             $(".gameIDtag").each(function (){
@@ -463,6 +460,7 @@ function submitWhiteCards(){
     var localRound = getRound();
     var cards = getSubmitCards();
     var roundID = localRound._id;
+    var guid = getGuid();
     if(localRound.czar != playerID){
         $.ajax({
             url: `${CONFIG_BASEURL}/v1/games/submitWhiteCard`,
@@ -470,7 +468,8 @@ function submitWhiteCards(){
             data: {
                 roundID: roundID,
                 whiteCards: cards,
-                playerID: playerID
+                playerID: playerID,
+                guid: guid
             },
             success: function( result ) {
                 doGameUpdate(result.data);
@@ -485,7 +484,9 @@ function submitWhiteCards(){
                 method: "POST",
                 data: {
                     roundID: localRound._id,
-                    player: czarCard
+                    player: czarCard,
+                    playerID: playerID,
+                    guid: guid
                 },
                 success: function( result ) {
                     updatePlayers(result.data.players, result.data.czar);
